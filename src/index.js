1c01c61bef46f67b5d9c4dae19be49a57130916f
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-15 06:12:55 -0500

    Remove strictMode and log missions to the console

--- src/index.js
+++ src/index.js
@@ -11,11 +11,9 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Missions = () => (
-  <div>
-    <h1>My Missions</h1>
-  </div>
-);
+const Missions = () => {
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v3/missions')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>My Missions</h1>
+    </div>
+  );
+};
 
 export default Missions;
