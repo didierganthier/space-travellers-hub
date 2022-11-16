1a54590e6ad4e74496fea1a141972bb1fe9b6fa9
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-16 06:23:59 -0500

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/configureStore';
 
 test('renders learn react link', () => {
   const tree = render(
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>,
   );
 
