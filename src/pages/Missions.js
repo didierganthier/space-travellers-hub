c37d6bf941b54873de97d9c0cc7e22f6e214e7ff
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-16 09:16:28 -0500

    Add styles to missions

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -17,41 +17,53 @@ const Missions = () => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th className="text-white">Empty</th>
-        </tr>
-      </thead>
-      {missions.map((mission) => (
-        <tbody
-          key={mission.mission_id}
-        >
+    <div className="overflow-x-auto relative shadow-md rounded-lg">
+      <table className="text-sm text-left text-gray-500 m-8 border">
+        <thead className="text-xs text-gray-700 uppercase bg-white">
           <tr>
-            <th>
-              {mission.mission_name}
+            <th scope="col" className="py-3 px-6 border">
+              Mission
             </th>
-            <th>
-              {mission.description}
+            <th scope="col" className="py-3 px-6 border">
+              Description
             </th>
-            <th>
-              {mission.joined ? 'Active Member' : 'NOT A MEMBER'}
+            <th scope="col" className="py-3 px-6 border">
+              Status
             </th>
-            <th>
-              <button
-                type="button"
-                onClick={() => handleClick(mission.mission_id)}
-              >
-                {mission.joined ? 'Leave Mission' : 'Join Mission'}
-              </button>
+            <th scope="col" className="py-3 border">
+              {' '}
             </th>
           </tr>
+        </thead>
+        <tbody className="mx-5">
+          {missions.map((mission, index) => (
+            <tr className={`${index % 2 === 0 ? 'bg-white' : 'bg-[#F2F2F2]'} border-b`} key={mission.mission_id}>
+              <th scope="row" className="py-4 px-6 font-medium border text-gray-900 whitespace-nowrap">
+                {mission.mission_name}
+              </th>
+              <td className="py-4 px-6 border w-2/3">
+                {mission.description}
+              </td>
+              <td className="py-4 px-6 border">
+                {mission.joined ? (
+                  <span className="bg-[#31A1B8] text-white text-xs p-2 rounded">Active Member</span>
+                ) : (
+                  <span className="bg-[#6D757D] text-white text-xs p-2 rounded">NOT A MEMBER</span>
+                )}
+              </td>
+              <td className="py-4 px-6 border">
+                {mission.joined ? (
+                  <button type="button" className="text-[#DD3B4A] border border-[#DD3B4A] text-xs p-2 rounded" onClick={() => handleClick(mission.mission_id)}>Leave Mission</button>
+                ) : (
+                  <button type="button" className="text-[#6D757D] border border-[#6D757D] text-xs p-2 rounded" onClick={() => handleClick(mission.mission_id)}>Join Mission</button>
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
-      ))}
-    </table>
+      </table>
+    </div>
+
   );
 };
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getMissions } from '../redux/missions/missionSlice';
 
@@ -6,20 +6,29 @@ const Profile = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.reducer.missions);
 
-  if (missions.length === 0) {
-    setTimeout(() => {
+  useEffect(() => {
+    // Check if a mission has been joined
+    if (missions.length === 0) {
       dispatch(getMissions());
-    }, '1000');
-  }
+    }
+  }, [dispatch, missions.length]);
+
+  const filteredMission = missions.filter((mission) => mission.joined === true);
 
   return (
-    <div>
-      <h1>My Missions</h1>
-      {missions.filter((mission) => mission.joined).map((mission) => (
-        <div key={mission.mission_id}>
-          <h2>{mission.mission_name}</h2>
+    <div className="mx-5">
+      <h1 className="font-bold my-5">My Missions</h1>
+      {filteredMission.length > 0 ? (
+        <div className="border w-1/2 rounded">
+          {missions.filter((mission) => mission.joined).map((mission) => (
+            <div key={mission.mission_id} className="border p-8">
+              <h2>{mission.mission_name}</h2>
+            </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        <p>No missions joined yet</p>
+      )}
     </div>
   );
 };
