77dab2821ebf5ae37214a37e7d19bba0369a44c4
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-16 05:54:59 -0500

    Filter joined missions

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/missions/missionSlice';
 
-const Profile = () => (
-  <>
-    <h2>My Profile</h2>
-  </>
-);
+const Profile = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.reducer.missions);
+
+  if (missions.length === 0) {
+    setTimeout(() => {
+      dispatch(getMissions());
+    }, '1000');
+  }
+
+  return (
+    <div>
+      <h1>My Missions</h1>
+      {missions.filter((mission) => mission.joined).map((mission) => (
+        <div key={mission.mission_id}>
+          <h2>{mission.mission_name}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Profile;
